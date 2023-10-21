e8ae1b1c449508774067b92726b3fc080db704ba
Londysiba <londekamhlengi.dlamini19@gmail.com>
2023-10-21 07:36:02 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,62 +1,25 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './App.css';
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 
-export default function LoginForm() {
-  let [city, setCity] = useState("");
-  const [loaded, setLoaded] = useState(false);
-  let [statement, setStatement] = useState("");
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="Cape Town" />
 
-  function showWeather(response) {
-    setLoaded(true);
-    setStatement({
-      temperature: Math.round(response.data.main.temp),
-      wind: Math.round(response.data.wind.speed),
-      humidity: Math.round(response.data.main.humidity),
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
-      description: response.data.weather[0].description
-    });
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    let apiKey = `6a48a550fc04f170639e60d52b8a6bc5`;
-    let units = "metric";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-    axios.get(apiUrl).then(showWeather);
-  }
-
-  function updateCity(event) {
-    setCity(event.target.value);
-  }
-
-  let form = (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="search"
-        placeholder="Search your city.."
-        onChange={updateCity}
-      />
-      <button type="Submit">Search</button>
-    </form>
-  );
-
-  if (loaded) {
-    return (
-      <div>
-        {form}
-        <ul>
-          <li>Temperature: {Math.round(statement.temperature)}°C</li>
-          <li>Description: {statement.description}</li>
-          <li>Humidity: {statement.humidity}%</li>
-          <li>Wind: {statement.wind}km/h</li>
-          <li>
-            <img src={statement.icon} alt={statement.description} />
-          </li>
-        </ul>
+        <footer>
+          
+          <a
+            href="https://github.com/Londysiba/weather_react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub repository
+          </a>{" "}
+          Coded by Londeka Dlamini
+        </footer>
       </div>
-    );
-  } else {
-    return form;
-  }
+    </div>
+  );
 }
